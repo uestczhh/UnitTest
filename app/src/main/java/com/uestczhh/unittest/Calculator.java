c9b15430c6422b45ca35571c3ef820fe381f45ad
zhh <1792252114@qq.com>
2016-10-26 10:14:05 +0800

    单元测试和UI测试

---
+++ app/src/main/java/com/uestczhh/unittest/Calculator.java
@@ -0,0 +1,24 @@
+package com.uestczhh.unittest;
+
+/**
+ * 被测试类
+ * Created by zhanghao on 2016/10/26.
+ */
+public class Calculator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double substract(double a, double b) {
+        return a-b;
+    }
+
+    public double divide(double a, double b) {
+        return a/b;
+    }
+
+    public double multiply(double a, double b) {
+        return a*b;
+    }
+}
